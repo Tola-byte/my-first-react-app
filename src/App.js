0630524e2eead91873d99a92d0cbe97e50f36450
Adetay1999 <adetayoomotomiwa99@gmail.com>
2022-03-18 19:53:10 +0100

    fixing bugs

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
-import './App.css';
-import Navbar from './components/Navbar.js';
-import  {BrowserRouter , Routes, Route , Link} from 'react-router-dom';
-import Home from './components/Home';
-
+import "./App.css";
+// import Navbar from './components/Navbar.js';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
 
 function App() {
   return (
-    <div className='App'>
-    <BrowserRouter>
-    <Routes>
-    <Route path="/Home" element={<Home />} />
-    </Routes>
-    </BrowserRouter>
-      </div>
-      
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
-
 
 export default App;
